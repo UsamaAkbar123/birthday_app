2aabd6d6bb354d5abf60bca25952f60d39e03ec9
usama-nexttier <usama.akbar@nexttier.io>
2023-07-06 10:39:52 +0500

    use implement keyword instead of with

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -1,80 +1,80 @@
 import 'package:birthdates/providers/navprovider.dart';
-import 'package:birthdates/utils/context.dart';
-import 'package:flutter/material.dart';
 import 'package:birthdates/utils/colors.dart';
+import 'package:birthdates/utils/context.dart';
 import 'package:birthdates/widgets/birthdates.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class MyAppBars extends StatelessWidget with PreferredSizeWidget {
-  const MyAppBars({
-    this.hasBackButton,
-    this.backTo,
-    super.key,
-  });
-  final bool? hasBackButton;
-  final int? backTo;
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      value: NavProvider.getInstance(),
-      child: AppBar(
-        backgroundColor: AppColors.background,
-        elevation: 0.1,
-        centerTitle: true,
-        title: const BirthDates(),
-        leading: Row(
-          children: [
-            const SizedBox(
-              width: 15,
-            ),
-            (hasBackButton == true)
-                ? InkWell(
-                    onTap: () {
-                      Provider.of<NavProvider>(context, listen: false)
-                          .setNavIndex(backTo ?? 1);
-                    },
-                    child: Image.asset(
-                      'assets/icons/back.png',
-                      scale: 2.95,
-                    ),
-                  )
-                : hasBackButton == false
-                    ? InkWell(
-                        onTap: () {
-                          Provider.of<NavProvider>(context, listen: false)
-                              .setNavIndex(backTo ?? 1);
-                        },
-                        child: Image.asset(
-                          'assets/icons/favourite.png',
-                          scale: 2.95,
-                        ),
-                      )
-                    : const SizedBox.shrink(),
-          ],
-        ),
-        actions: [
-          InkWell(
-            onTap: () {
-              Provider.of<NavProvider>(context, listen: false).setNavIndex(3);
-            },
-            child: Image.asset(
-              'assets/icons/settings.png',
-              scale: 2.95,
-            ),
-          ),
-          const SizedBox(
-            width: 15,
-          ),
-        ],
-      ),
-    );
-  }
+// class MyAppBars extends StatelessWidget with PreferredSizeWidget {
+//   const MyAppBars({
+//     this.hasBackButton,
+//     this.backTo,
+//     super.key,
+//   });
+//   final bool? hasBackButton;
+//   final int? backTo;
+//   @override
+//   Widget build(BuildContext context) {
+//     return ChangeNotifierProvider.value(
+//       value: NavProvider.getInstance(),
+//       child: AppBar(
+//         backgroundColor: AppColors.background,
+//         elevation: 0.1,
+//         centerTitle: true,
+//         title: const BirthDates(),
+//         leading: Row(
+//           children: [
+//             const SizedBox(
+//               width: 15,
+//             ),
+//             (hasBackButton == true)
+//                 ? InkWell(
+//                     onTap: () {
+//                       Provider.of<NavProvider>(context, listen: false)
+//                           .setNavIndex(backTo ?? 1);
+//                     },
+//                     child: Image.asset(
+//                       'assets/icons/back.png',
+//                       scale: 2.95,
+//                     ),
+//                   )
+//                 : hasBackButton == false
+//                     ? InkWell(
+//                         onTap: () {
+//                           Provider.of<NavProvider>(context, listen: false)
+//                               .setNavIndex(backTo ?? 1);
+//                         },
+//                         child: Image.asset(
+//                           'assets/icons/favourite.png',
+//                           scale: 2.95,
+//                         ),
+//                       )
+//                     : const SizedBox.shrink(),
+//           ],
+//         ),
+//         actions: [
+//           InkWell(
+//             onTap: () {
+//               Provider.of<NavProvider>(context, listen: false).setNavIndex(3);
+//             },
+//             child: Image.asset(
+//               'assets/icons/settings.png',
+//               scale: 2.95,
+//             ),
+//           ),
+//           const SizedBox(
+//             width: 15,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+//
+//   @override
+//   Size get preferredSize => const Size.fromHeight(70);
+// }
 
-  @override
-  Size get preferredSize => const Size.fromHeight(70);
-}
-
-class MyAppBar extends StatelessWidget with PreferredSizeWidget {
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   const MyAppBar({
     this.hasBackButton,
     this.backTo,
