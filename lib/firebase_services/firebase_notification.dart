a57aa7fdb140e942b53383f845049196ed2c9fcd
Usama <usamatahirsatti@gmail.com>
2023-08-14 17:06:28 +0500

    now i can recieved the notification on app terminated and background state

--- lib/firebase_services/firebase_notification.dart
+++ lib/firebase_services/firebase_notification.dart
@@ -2,7 +2,7 @@ import 'package:birthdates/managers/preference_manager.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 Future<void> handleBackgroundMessage(RemoteMessage message) async {
-  print(message.notification?.title);
+  // print(message.notification?.title);
 }
 
 class FirebaseNotification {
@@ -19,6 +19,18 @@ class FirebaseNotification {
     await _firebaseMessaging.requestPermission();
     // final fcmToken = preference.getDeviceToken;
     // print(fcmToken);
+
+    /// get notification when app is in background state
     FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+
+    /// get notification when app is in foreground state
+    FirebaseMessaging.onMessage.listen((RemoteMessage remoteMessage) {
+      // print('Got a message whilst in the foreground!');
+      // print('Message data: ${remoteMessage.data}');
+      // if (remoteMessage.notification != null) {
+      //   print(
+      //       'Message also contained a notification: ${remoteMessage.notification}');
+      // }
+    });
   }
 }
