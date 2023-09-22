8bea9fb2aa799111a0903188c5c31d9720360658
Muhammad <arslan.maher@nexttier.io>
2023-09-22 15:49:02 +0500

    revert the logic of days calculation on home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -151,9 +151,9 @@ class _MainHomeCardState extends State<MainHomeCard> {
           widget.birthdayModel.dob.day,
         );
         final difference = currentYearDataTime.difference(nextBirthday);
-        // days = (difference.inDays).toString();
-        int res = (difference.inSeconds / 86400).truncate();
-        days = res == 0 ?  res.toString() : (res - 1).toString();
+        days = (difference.inDays).toString();
+        // int res = (difference.inSeconds / 86400).truncate();
+        // days = res == 0 ?  res.toString() : (res - 1).toString();
         setState(() {});
       } else {
         currentYearDataTime = DateTime(
@@ -162,9 +162,9 @@ class _MainHomeCardState extends State<MainHomeCard> {
           widget.birthdayModel.dob.day,
         );
         final difference = currentYearDataTime.difference(nextBirthday);
-        // days = (difference.inDays).toString();
-        int res = (difference.inSeconds / 86400).truncate();
-        days = res == 0 ?  res.toString() : (res - 1).toString();
+        days = (difference.inDays).toString();
+        // int res = (difference.inSeconds / 86400).truncate();
+        // days = res == 0 ?  res.toString() : (res - 1).toString();
         setState(() {});
       }
 
